4887e63027abd5ca9ff5036a3d19de67d5e99bf3
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2024-11-20 19:44:07 +0700

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   credits.txt 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/assets/models/airplane.glb 	new file:   public/assets/models/scene.glb 	new file:   public/assets/textures/envmap.hdr 	new file:   public/assets/textures/envmap.jpg 	new file:   server/server.js 	new file:   src/Airplane.jsx 	new file:   src/App.jsx 	new file:   src/Landscape.jsx 	new file:   src/MotionBlur.jsx 	new file:   src/SphereEnv.jsx 	new file:   src/Targets.jsx 	new file:   src/controls.js 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ server/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // React app
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // Mendengarkan pesan dari client
+  socket.on("send_message", (data) => {
+    console.log("Message received:", data);
+    // Kirim pesan ke semua client
+    io.emit("receive_message", "hello World");
+  });
+
+  setInterval(() => {
+    // Kirim pesan ke semua client
+    io.emit("receive_message", "d");
+  }, 1000);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
